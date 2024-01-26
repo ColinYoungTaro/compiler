10e6792a8be00d123fab0fe59592a073771b2626
ColinYoungTaro <colin.young.taro@gmail.com>
2024-01-26 15:14:45 +0800

    detail

--- main.cpp
+++ main.cpp
@@ -13,21 +13,11 @@
 
 using namespace std;
 
-// 声明 lexer 的输入, 以及 parser 函数
-// 为什么不引用 sysy.tab.hpp 呢? 因为首先里面没有 yyin 的定义
-// 其次, 因为这个文件不是我们自己写的, 而是被 Bison 生成出来的
-// 你的代码编辑器/IDE 很可能找不到这个文件, 然后会给你报错 (虽然编译不会出错)
-// 看起来会很烦人, 于是干脆采用这种看起来 dirty 但实际很有效的手段
 extern FILE *yyin;
 extern int yyparse(unique_ptr<BaseAST> &ast);
 
 int main(int argc, const char *argv[]) {
-  // 解析命令行参数. 测试脚本/评测平台要求你的编译器能接收如下参数:
-  // compiler 模式 输入文件 -o 输出文件
-  // assert(argc == 5);
-  // auto mode = argv[1];
-  // auto input = argv[2];
-  // auto output = argv[4];
+
   auto input = argv[1];
   // 打开输入文件, 并且指定 lexer 在解析的时候读取这个文件
   yyin = fopen(input, "r");
@@ -38,9 +28,6 @@ int main(int argc, const char *argv[]) {
   auto ret = yyparse(ast);
   assert(!ret);
 
-  // ast->dump(cout);
-  // CodeGenVisitor visitor(*context);
-  // ast->accept(visitor);
   ctx = new CodeGenContext();
   auto& context = ctx->getContextRef();
   std::vector<Type*> printf_arg_types;
@@ -48,7 +35,7 @@ int main(int argc, const char *argv[]) {
   FunctionType* printf_type = FunctionType::get(Type::getVoidTy(context), printf_arg_types, true);
   ctx->theModule->getOrInsertFunction("printInt", printf_type);
 
-  // 获取或插入一个scanf函数声明
+  // 插入对runtime中函数的声明
   FunctionType* scanf_type = FunctionType::get(Type::getInt32Ty(context), true);
   ctx->theModule->getOrInsertFunction("getInt", scanf_type);
   FunctionType* printLnTy = FunctionType::get(Type::getVoidTy(context), true);
--- parser/sysy.y
+++ parser/sysy.y
@@ -39,18 +39,10 @@ void copyList(ElmList<T>& target, RawAstPtrList<K>* list) {
 
 %}
 
-// 定义 parser 函数和错误处理函数的附加参数
-// 我们需要返回一个字符串作为 AST, 所以我们把附加参数定义成字符串的智能指针
-// 解析完成后, 我们要手动修改这个参数, 把它设置成解析得到的字符串
 %parse-param { 
   std::unique_ptr<BaseAST> &ast 
 }
 
-// yylval 的定义, 我们把它定义成了一个联合体 (union)
-// 因为 token 的值有的是字符串指针, 有的是整数
-// 之前我们在 lexer 中用到的 str_val 和 int_val 就是在这里被定义的
-// 至于为什么要用字符串指针而不直接用 string 或者 unique_ptr<string>?
-// 请自行 STFW 在 union 里写一个带析构函数的类会出现什么情况
 %union {
   BaseAST* ast_val;
   RawAstPtrList<BaseAST> *item_list;
